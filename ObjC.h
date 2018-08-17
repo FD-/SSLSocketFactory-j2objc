1faafe14631a8494af34c2549506da63e026fb60
FD- <fdraschbacher@gmail.com>
2018-08-17 12:38:41 +0200

    Create ObjC.h

---
+++ ObjC.h
@@ -0,0 +1,26 @@
+///
+/// ObjC.h
+///
+/// @author Florian Draschbacher
+/// @copyright © 2018 Florian Draschbacher. All rights reserved.
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+/// http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+#import <Foundation/Foundation.h>
+
+@interface ObjC : NSObject
+
++ (BOOL)catchException:(void(^)())tryBlock error:(__autoreleasing NSError **)error;
++ (void)throwException:(id)exception;
+
+@end
